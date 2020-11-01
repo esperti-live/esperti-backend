7385a2ed66d91ed0127374221ca10126f6dba466
Entreprenerd <valeranijr@gmail.com>
2020-11-01 11:20:58 +0100

    feat with pg-connection-string

--- config/database.js
+++ config/database.js
@@ -1,15 +1,42 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL)
+
+module.exports = ({ env }) => {
+  if(env('NODE_ENV') === 'production'){
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
+    }
+  } else {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
       },
-    },
-  },
-});
+    }
+  }
+};
