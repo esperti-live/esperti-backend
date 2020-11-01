835bf84ac8902fd24882eecb93a887ef465b70a2
Entreprenerd <valeranijr@gmail.com>
2020-11-01 12:22:54 +0100

    fix skills are by profile

--- api/skill/controllers/skill.js
+++ api/skill/controllers/skill.js
@@ -13,13 +13,13 @@ module.exports = {
      * TODO: Test with many filters to try using OR queries to leak other peoples skills
      */
     async find(ctx) {
-        const {user} = ctx.state
+        const {profile} = ctx.state.user
         
         let entities;
         if (ctx.query._q) {
-          entities = await strapi.services.skill.search({...ctx.query, user: user.id}, []);
+          entities = await strapi.services.skill.search({...ctx.query, profile: user.id}, []);
         } else {
-          entities = await strapi.services.skill.find({...ctx.query, user: user.id}, []);
+          entities = await strapi.services.skill.find({...ctx.query, profile: user.id}, []);
         }
     
         return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.skill }));
