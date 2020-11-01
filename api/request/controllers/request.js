58b292247f6ee7cdd897855bde8542735ede9ae3
Entreprenerd <valeranijr@gmail.com>
2020-11-01 12:08:52 +0100

    feat request api + aws upload

--- api/request/controllers/request.js
+++ api/request/controllers/request.js
@@ -1,8 +1,47 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    /**
+     * Retrieve all requests
+     * Purposefully no populate so you only get basic data
+     * May break user data. Let's finesse later
+     */
+    async find(ctx) {
+        let entities;
+        if (ctx.query._q) {
+        entities = await strapi.services.restaurant.search(ctx.query, []);
+        } else {
+        entities = await strapi.services.restaurant.find(ctx.query, []);
+        }
+
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.restaurant }));
+    },
+    /**
+     * Retrieve one request, by slug
+     * @param {*} ctx 
+     */
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.services.services.findOne({ slug: id });
+        return sanitizeEntity(entity, { model: strapi.models.services });
+    },
+    /**
+     * Create a new requst
+     * Your request is automatically attached to your profile
+     */
+    async create(ctx) {
+        const {profile} = ctx.state.user.profile
+        console.log("request.create profile", profile)
+
+        const entity = await strapi.services.request.create({...ctx.request.body, profile});
+        return sanitizeEntity(entity, { model: strapi.models.request });
+    },
+};
