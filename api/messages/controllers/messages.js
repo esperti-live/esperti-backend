a539b21846c1c08671fa5ad685b5b91c38bb9aea
Entreprenerd <valeranijr@gmail.com>
2020-11-03 15:28:26 +0100

    feat sendToRequest

--- api/messages/controllers/messages.js
+++ api/messages/controllers/messages.js
@@ -29,5 +29,30 @@ module.exports = {
             html: message
         });
         return {status: "Ok"}
+    },
+    sendToRequest: async (ctx) => {
+        const {        
+            message
+        } = ctx.request.body
+
+        const { id } = ctx.params;
+
+        const from = ctx.state.user.email
+
+        const targetProfile = await strapi.services.request.findOne({id})
+        console.log("targetProfile", targetProfile)
+        const to = targetProfile.user.email
+
+        await strapi.plugins['email'].services.email.send({
+            to,
+            from: 'strapi@esperti.live',
+            cc: 'strapi@esperti.live',
+            bcc: 'alex@esperti.live',
+            replyTo: from,
+            subject: 'You received a message from Esperti!',
+            text: message,
+            html: message
+        });
+        return {status: "Ok"}
     }
 };
