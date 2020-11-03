903b94279968204cb2cf260f06a5c12db40d9762
Entreprenerd <valeranijr@gmail.com>
2020-11-03 15:36:23 +0100

    fix slugify bug

--- api/profile/controllers/profile.js
+++ api/profile/controllers/profile.js
@@ -1,6 +1,6 @@
 'use strict';
 const { sanitizeEntity } = require('strapi-utils');
-
+const slugify = require('slugify')
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
